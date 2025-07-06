218a0e461284c4822e134c7081dbee1ca03a848b
LucasRP <rodrigues.lrp21@gmail.com>
2025-07-06 18:58:30 -0300

    feat: desenvolvimento da entidade Usuario

--- backend/src/model/Usuario.js
+++ backend/src/model/Usuario.js
@@ -4,6 +4,38 @@ const sequelize = require('../config/database');
 
 const Usuario = sequelize.define('Usuario',
     {
+        id: {
+            type : DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
 
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        login : {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+
+        senha : {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        qntd_estrelas : {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+        },
+    },
+
+    {
+        tableName: 'Usuario',
+        timestamps: true,
     }
-)
+);
+
+modeule.exports = Usuario;
