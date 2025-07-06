47921ec7d026c2df018f5b6b518c4c1d2f7b66b8
LucasRP <rodrigues.lrp21@gmail.com>
2025-07-06 18:28:22 -0300

    feat: add de todas as entidades do projeto

---
+++ backend/src/model/Categoria.js
@@ -0,0 +1,9 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../config/database');
+
+
+const Categoria = sequelize.define('Categoria',
+    {
+
+    }
+)
---
+++ backend/src/model/Materia.js
@@ -0,0 +1,9 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../config/database');
+
+
+const Materia = sequelize.define('Materia',
+    {
+
+    }
+)
