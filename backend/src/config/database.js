eddbbf34e69781a5d78572147000ea6a6ef82b32
LucasRP <rodrigues.lrp21@gmail.com>
2025-07-07 14:38:20 -0300

    feat: ajustes para rodar o servidor e conectar ao db

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,4 @@
-const {sequelize, Sequelize} = require('sequelize');
+const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,29 @@
+const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '..', '.env')});
 
-
 const express = require('express');
-const app = express();
 
-app.listen(4444, () =>{
-    console.log('servidor rodando: http://localhost:4444');
-})
+const sequelize = require('./config/database');
+require('./config/database');
+
+const app = express();
+app.use(express.json());
+const PORT = 5555;
+
+
+(async () =>{
+    try{
+
+        await sequelize.sync({alter:true});
+
+        console.log("Sicronizado com sucesso!");
+
+        app.listen(PORT, () =>{
+            console.log(` Servidor rodando em: http://localhost:${PORT}`);
+            });
+    }
+    catch( error){
+        console.log("Falha ao sicronizar: " + error);
+    };
+    
+}) ();
