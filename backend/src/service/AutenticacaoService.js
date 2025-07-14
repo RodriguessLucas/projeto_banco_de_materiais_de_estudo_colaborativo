3e7cdf8bcc1cdb061b811bd8f150fbaa336b8eec
LucasRP <rodrigues.lrp21@gmail.com>
2025-07-14 16:28:59 -0300

    feat: criação de servico de autenticação de login

---
+++ backend/src/service/AutenticacaoService.js
@@ -0,0 +1,33 @@
+const Usuario = require('../model/Usuario');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { where } = require('sequelize');
+
+class AutenticacaoService{
+    async login(email, senha){
+
+        const usuario = await Usuario.findOne( {where: {email} } );
+        if(!usuario){
+            throw new Error('E-mail ou senha inválido! Tente novamente');
+        }
+
+        const senha = await bcrypt.compare(senha, usuario.senha);
+        if(!senha){
+            throw new Error('E-mail ou senha inválido! Tente novamente');
+        }
+
+        const token = jwt.sign(
+            {id: usuario.id},
+            process.env.JWT_SECRET,
+            {expiresIn: '4h'}
+        );
+
+        usuario.senha = undefined;
+
+        return {usuario, token};
+
+    }
+
+}
+
+module.exports = new AutenticacaoService();
