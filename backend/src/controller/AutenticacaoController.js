48f97ad6ed4f2e6c8e3e4caf175adce638a70a48
LucasRP <rodrigues.lrp21@gmail.com>
2025-07-14 16:35:17 -0300

    feat: add controller de autenticação

---
+++ backend/src/controller/AutenticacaoController.js
@@ -0,0 +1,20 @@
+const { Model } = require('sequelize');
+const AutenticacaoService = require('../service/AutenticacaoService');
+
+class AutenticacaoController{
+
+    async login(req, res){
+        try{
+            const {email, senha} = req.body;
+            const resultado = await AutenticacaoService.login(email, senha);
+
+            return res.status(200).json(resultado);
+        }
+        catch(error){
+            return res.status(401).json({message: error.message});
+        }
+
+    }
+}
+
+module.exports = new AutenticacaoController();
