48e32bfd2682f45d19ecb0aeeced4c34c6be5a09
guilherdrk <guibem20@gmail,com>
2025-07-15 15:21:28 -0300

    feat: implementando rota perfil obterPerfilLogado e meuPerfil

--- backend/src/service/UsuarioService.js
+++ backend/src/service/UsuarioService.js
@@ -1,29 +1,28 @@
 const { toCadastroRes } = require('../dto/UsuarioDTO');
-const Usuario = require('../model/Usuario');
-const bcrypt  = require('bcryptjs');
+const Usuario = require("../model/Usuario");
+const Material = require("../model/Material");
+const bcrypt = require("bcryptjs");
 
-class UsuarioService{
+class UsuarioService {
+  async criarUsuario(dadosUsuario) {
+    const { login, senha } = dadosUsuario;
 
-    async criarUsuario(dadosUsuario){
-        const{ login, senha} = dadosUsuario;
-
-        const usuarioExiste = await Usuario.findOne({where:{login}});
-        if(usuarioExiste){
-            throw new Error("Email em uso!");
-        }
+    const usuarioExiste = await Usuario.findOne({ where: { login } });
+    if (usuarioExiste) {
+      throw new Error("Email em uso!");
+    }
 
         const senhaHash = await bcrypt.hash(senha, 10);
+
 
         const novoUsuario = await Usuario.create({
             ...dadosUsuario,
             senha: senhaHash
         });
-        
-        const usuarioDTO = toCadastroRes(novoUsuario);
 
-        const message = 'Usuário cadastrado com sucesso!';
-        return {usuarioDTO, message};
 
+        novoUsuario.senha = undefined;
+        return novoUsuario;
     }
 
 }
