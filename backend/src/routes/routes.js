0edfa8abe18c0c72491c7e678c227974a71c14c3
LucasRP <rodrigues.lrp21@gmail.com>
2025-07-16 10:20:22 -0300

    feat: add rota para cadastrar materiais

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('../config/upload');
+
+
 const UsuarioController = require('../controller/UsuarioController');
 const AutenticacaoController = require('../controller/AutenticacaoController');
+const MaterialController = require('../controller/MaterialController');
+
+
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 
 routes.post('/cadastrarUsuario', UsuarioController.criar);
-
 routes.post('/login', AutenticacaoController.login);
+routes.post('/usuario/cadastrarMateriais', upload.single('arquivo'), MaterialController.criar);
+
 
 module.exports = routes;
